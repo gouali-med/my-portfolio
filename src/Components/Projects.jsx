7b4ff70719bb03d1199ca5d36d3ed4870b3e9b7e
gouali-med <mohammed.gouali@gmail.Com>
2025-03-11 22:59:59 +0000

    modify projects and add gsm exp

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -25,9 +25,9 @@ function Portfolio() {
         <div class="col-lg-4 col-md-6 portfolio-item filter-web">
             <div class="portfolio-wrap">
               <h5 className="text-center text-white" style={{backgroundColor:"#149ddd"}}>SaaS pos software</h5>
-              <img src="../../img\portfolio\pos.png" class="img-fluid" alt=""/>
+              <img src="/img\portfolio\pos.png" class="img-fluid" alt=""/>
               <div class="portfolio-links">
-                <a href="../../img\portfolio\pos.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
+                <a href="/img\portfolio\pos.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
                 <a target="_blank" href="https://github.com/gouali-med/" title="More Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -36,9 +36,9 @@ function Portfolio() {
           <div class="col-lg-4 col-md-6 portfolio-item filter-web">
             <div class="portfolio-wrap">
               <h5 className="text-center text-white" style={{backgroundColor:"#149ddd"}}>antiques</h5>
-              <img src="../../img\portfolio\antiques.png" class="img-fluid" alt=""/>
+              <img src="/img\portfolio\antiques.png" class="img-fluid" alt=""/>
               <div class="portfolio-links">
-                <a href="../../img\portfolio\antiques.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
+                <a href="/img\portfolio\antiques.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
                 <a target="_blank" href="https://github.com/gouali-med/" title="More Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -47,9 +47,9 @@ function Portfolio() {
           <div class="col-lg-4 col-md-6 portfolio-item filter-web">
             <div class="portfolio-wrap">
               <h5 className="text-center text-white" style={{backgroundColor:"#149ddd"}}>Viva Solutions</h5>
-              <img src="../../img\portfolio\home.png" class="img-fluid" alt=""/>
+              <img src="/img\portfolio\home.png" class="img-fluid" alt=""/>
               <div class="portfolio-links">
-                <a href="../../img\portfolio\home.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
+                <a href="/img\portfolio\home.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
               </div>
             </div>
           </div>
@@ -57,9 +57,9 @@ function Portfolio() {
           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
             <div class="portfolio-wrap">
             <h5 className="text-center text-white" style={{backgroundColor:"#149ddd"}}>ecommerce website</h5>
-              <img src="../../img/portfolio/Captureecommerce.png" class="img-fluid" alt=""/>
+              <img src="/img/portfolio/Captureecommerce.png" class="img-fluid" alt=""/>
               <div class="portfolio-links">
-                <a href="../../img/portfolio/Captureecommerce.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
+                <a href="/img/portfolio/Captureecommerce.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
                 <a target="_blank" href="https://github.com/gouali-med/" title="More Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -68,9 +68,9 @@ function Portfolio() {
           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
             <div class="portfolio-wrap">
             <h5 className="text-center text-white" style={{backgroundColor:"#149ddd"}}>ecommerce plombietools</h5>
-              <img src="../../img/portfolio/plombie.png" class="img-fluid" alt=""/>
+              <img src="/img/portfolio/plombie.png" class="img-fluid" alt=""/>
               <div class="portfolio-links">
-                <a href="../../img/portfolio/plombie.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
+                <a href="/img/portfolio/plombie.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
                 <a target="_blank" href="https://github.com/gouali-med/" title="More Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -79,9 +79,9 @@ function Portfolio() {
           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
             <div class="portfolio-wrap">
             <h5 className="text-center text-white" style={{backgroundColor:"#149ddd"}}>ecommerce website</h5>
-              <img src="../../img/portfolio/Captureecommerce.png" class="img-fluid" alt=""/>
+              <img src="/img/portfolio/Captureecommerce.png" class="img-fluid" alt=""/>
               <div class="portfolio-links">
-                <a href="../../img/portfolio/Captureecommerce.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
+                <a href="/img/portfolio/Captureecommerce.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
                 <a target="_blank" href="https://github.com/gouali-med/" title="More Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
