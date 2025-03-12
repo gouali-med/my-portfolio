d62485163568e8c462a45dddb1b5ff1b0b42172e
Mohamed GOUALI <73234744+gouali-med@users.noreply.github.com>
2025-03-12 22:09:38 +0000

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -57,9 +57,9 @@ function Portfolio() {
           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
             <div class="portfolio-wrap">
             <h5 className="text-center text-white" style={{backgroundColor:"#149ddd"}}>ecommerce website</h5>
-              <img src="/img/portfolio/Captureecommerce.png" class="img-fluid" alt=""/>
+              <img src="/img/portfolio\Captureecommerce.png" class="img-fluid" alt=""/>
               <div class="portfolio-links">
-                <a href="/img/portfolio/Captureecommerce.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
+                <a href="/img/portfolio\Captureecommerce.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
                 <a target="_blank" href="https://github.com/gouali-med/" title="More Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -68,9 +68,9 @@ function Portfolio() {
           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
             <div class="portfolio-wrap">
             <h5 className="text-center text-white" style={{backgroundColor:"#149ddd"}}>ecommerce plombietools</h5>
-              <img src="/img/portfolio/plombie.png" class="img-fluid" alt=""/>
+              <img src="/img/portfolio\plombie.png" class="img-fluid" alt=""/>
               <div class="portfolio-links">
-                <a href="/img/portfolio/plombie.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
+                <a href="/img/portfolio\plombie.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
                 <a target="_blank" href="https://github.com/gouali-med/" title="More Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
@@ -79,9 +79,9 @@ function Portfolio() {
           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
             <div class="portfolio-wrap">
             <h5 className="text-center text-white" style={{backgroundColor:"#149ddd"}}>ecommerce website</h5>
-              <img src="/img/portfolio/Captureecommerce.png" class="img-fluid" alt=""/>
+              <img src="/img/portfolio\Captureecommerce.png" class="img-fluid" alt=""/>
               <div class="portfolio-links">
-                <a href="/img/portfolio/Captureecommerce.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
+                <a href="/img/portfolio\Captureecommerce.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
                 <a target="_blank" href="https://github.com/gouali-med/" title="More Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
