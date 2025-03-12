682cf412366450eb1e2c2123bfeb4fdf3ac7166d
Mohamed GOUALI <73234744+gouali-med@users.noreply.github.com>
2025-03-12 22:42:36 +0000

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
 <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
   <div class="hero-container" >
     <h1>Mohamed Gouali</h1>
-    <p> Developer & Freelancer  <span className="typed"  data-typed-items=" Developer, Freelancer"></span></p>
+    <p> Fullstack Dotnet /Angular Developer  <span className="typed"  data-typed-items=" Developer, Freelancer"></span></p>
   </div>
 </section>
 
